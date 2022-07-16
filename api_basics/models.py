ebb9741caff787e97e9722d3f26181ef8ed25489
glenchiridza <glenchiridz@gmail.com>
2022-07-16 12:53:10 +0200

    course, review model instances

--- api_basics/models.py
+++ api_basics/models.py
@@ -9,3 +9,26 @@ DATABASE = SqliteDatabase('courses.sqlite')
 class Course(Model):
     title = CharField()
     url = CharField(unique=True)
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
+
+class Review(Model):
+    course = ForeignKeyField(Course, related_name='review_set')
+    rating = IntegerField()
+    comment = TextField(default='')
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Course, Review], safe=True) # safe=True is so that it jus passes if those tables already
+    # exist in DB instead of error
+    DATABASE.close()
+
+
