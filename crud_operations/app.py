f06e702afb04b2a2180e1d4095cea890c79658ac
glenchiridza <glenchiridz@gmail.com>
2022-07-16 11:09:50 +0200

    remove record

--- crud_operations/app.py
+++ crud_operations/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,10 +21,10 @@ all_posts = [
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100),nullable=False)
-    content = db.Column(db.Text,nullable=False)
-    author = db.Column(db.String(50),nullable=False,default='N/A')
-    date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(50), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f'Blog post == {self.title}'
@@ -36,18 +36,27 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/posts/',methods=['GET','POST'])
+@app.route('/posts/', methods=['GET', 'POST'])
 def posts():
     if request.method == 'POST':
         post_title = request.form['title']
+        post_author = request.form['author']
         post_content = request.form['content']
-        new_post = BlogPost(title=post_title, content=post_content, author='Glen')
+        new_post = BlogPost(title=post_title, author=post_author, content=post_content)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts/')
     else:
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
     return render_template('posts.html', posts=all_posts)
+
+
+@app.route('posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts/')
 
 
 if __name__ == "__main__":
